d60481f7ce1e109d16cdf5c8edefa68b2f353065
apha12 <aboubakar.ouattara@centrale-casablanca.ma>
2022-11-03 16:59:59 +0100

    update vessel

--- Vaisseaux.py
+++ Vaisseaux.py
@@ -1,26 +1,29 @@
 from Armes import Weapon
 
 class Vessel:
-    def __init__(self,coordinates : tuple, max_hits : int, weapon : Weapon):
-        self._coordinates = coordinates
-        self._max_hits = max_hits
-        self._weapon = Weapon
+    def __init__(self, coordinates: tuple, max_hits: int, weapon: Weapon):
+        self._coordinates=coordinates
+        self._max_hits= max_hits
+        self._weapon= weapon
+    
+    
+        
+    def go_to(self, x: int, y:int, z:int):
+        self._coordinates= [x, y, z]
+       
 
-    def go_to(self,x:int,y:int,z:int):
-        self._coordinates = [x,y,z]
-
-    def get_coordinates(self,int,int,int):
+    def get_coordinate(self,x, y,z):
+        self._x=x
+        self._y=y
+        self._z=z
 
     def fire_at(self,x:int,y:int,z:int):
-        try:
-            try:
-
-            except
-                print("OutOfRange")
-                Weapon._ammunitions = Weapon._ammunitions - 1
-        except self._max_hits ==0:
+        if self._max_hits==0:
             print("DestroyedError")
-
+        elif self._coordinates> Weapon.range:
+            print("OutOfRangeError")
+            Weapon._ammunitions-=1
+            
 class Cruiser(Vessel):
     def go_to(self,x:int,y:int,z:int):
         try:
