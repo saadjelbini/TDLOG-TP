dc457c95bff507a7734678624d0ff3118c36474d
apha12 <aboubakar.ouattara@centrale-casablanca.ma>
2022-11-03 16:09:01 +0100

    Modification classe arme

--- Armes.py
+++ Armes.py
@@ -10,47 +10,56 @@ class Weapon:
         self._z=z
 
 
-class Missile_anti_surface(Weapon):
+class Lance_missile_anti_surface(Weapon):
     def __init__(self, ammunitions=40, range=30):
         super().__init__(ammunitions, range)
 
     def fire_at(x:int,y:int,z:int):
-        try:
-            try:
-                print(f"feu sur le point {x},{y},{z}")
-                Weapon._ammunitions = Weapon._ammunitions - 1
-            except Weapon._ammunitions == 0 :
-                print("NoAmmunationError")
-        except z != 0:
-            print("OutOfRangeError")
-            Weapon._ammunitions = Weapon._ammunitions - 1
 
-class Missile_anti_air(Weapon):
+        if Weapon._ammunitions==0:
+            print("NoAmmunationError")
+
+        elif z!=0:
+            print("OutofRange")
+            Weapon._ammunitions = Weapon._ammunitions - 1
+        
+        else:
+            print(f"feu sur le point {x},{y},{z}")
+                
+        
+        
+
+class Lance_missile_anti_air(Weapon):
     def __init__(self, ammunitions=50, range=40):
         super().__init__(ammunitions, range)
 
     def fire_at(x:int,y:int,z:int):
-        try:
-            try:
-                print(f"feu sur le point {x},{y},{z}")
-                Weapon._ammunitions = Weapon._ammunitions - 1
-            except Weapon._ammunitions == 0 :
-                print("NoAmmunationError")
-        except z <=0 :
-            print("OutOfRangeError")
-            Weapon._ammunitions = Weapon._ammunitions - 1
+        if Weapon._ammunitions==0:
+            print("NoAmmunationError")
 
-class Torpille(Weapon):
+        elif z<=0:
+            print("OutofRange")
+            Weapon._ammunitions = Weapon._ammunitions - 1
+        
+        else:
+            print(f"feu sur le point {x},{y},{z}")
+                
+        
+            
+class Lance_Torpille(Weapon):
     def __init__(self, ammunitions=15, range=20):
         super().__init__(ammunitions, range)
 
     def fire_at(x:int,y:int,z:int):
-        try:
-            try:
-                print(f"feu sur le point {x},{y},{z}")
-                Weapon._ammunitions = Weapon._ammunitions - 1
-            except Weapon._ammunitions == 0 :
-                print("NoAmmunationError")
-        except z >0 :
-            print("OutOfRangeError")
+
+        if Weapon._ammunitions==0:
+            print("NoAmmunationError")
+
+        elif z>0:
+            print("OutofRange")
             Weapon._ammunitions = Weapon._ammunitions - 1
+        
+        else:
+            print(f"feu sur le point {x},{y},{z}")
+                
+        
