9c02cdfb14903240b3387d54e55350d3e065af9b
apha12 <aboubakar.ouattara@centrale-casablanca.ma>
2022-11-03 15:03:00 +0100

    spécification des armes

--- Armes.py
+++ Armes.py
@@ -4,7 +4,16 @@ class Weapon:
         self._ammunitions = ammunitions
         self._range = range
 
+    def fire_at(self, x:int, y:int, z:int):
+        self._x=x
+        self._y=y
+        self._z=z
+
+
 class Missile_anti_surface(Weapon):
+    def __init__(self, ammunitions=40, range=30):
+        super().__init__(ammunitions, range)
+
     def fire_at(x:int,y:int,z:int):
         try:
             try:
@@ -13,10 +22,13 @@ class Missile_anti_surface(Weapon):
             except Weapon._ammunitions == 0 :
                 print("NoAmmunationError")
         except z != 0:
-            print("OutOFRangeError")
+            print("OutOfRangeError")
             Weapon._ammunitions = Weapon._ammunitions - 1
 
 class Missile_anti_air(Weapon):
+    def __init__(self, ammunitions=50, range=40):
+        super().__init__(ammunitions, range)
+
     def fire_at(x:int,y:int,z:int):
         try:
             try:
@@ -25,10 +37,13 @@ class Missile_anti_air(Weapon):
             except Weapon._ammunitions == 0 :
                 print("NoAmmunationError")
         except z <=0 :
-            print("OutOFRangeError")
+            print("OutOfRangeError")
             Weapon._ammunitions = Weapon._ammunitions - 1
 
 class Torpille(Weapon):
+    def __init__(self, ammunitions=15, range=20):
+        super().__init__(ammunitions, range)
+
     def fire_at(x:int,y:int,z:int):
         try:
             try:
@@ -37,5 +52,5 @@ class Torpille(Weapon):
             except Weapon._ammunitions == 0 :
                 print("NoAmmunationError")
         except z >0 :
-            print("OutOFRangeError")
+            print("OutOfRangeError")
             Weapon._ammunitions = Weapon._ammunitions - 1
