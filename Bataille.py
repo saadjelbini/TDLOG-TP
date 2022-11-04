1fbc478374005c56ac77871f7c32b9283c9bf2f5
ER-ROUGUI <114986162+ER-ROUGUI@users.noreply.github.com>
2022-11-04 17:04:54 +0100

    Update Bataille.py

---
+++ Bataille.py
@@ -0,0 +1,14 @@
+
+from Armes import*
+from vaisseaux import *
+
+class espace_jeux():
+    def __init__(self,x,y,z,list_vaisseaux):
+        self.x =x
+        self.y =y
+        self.z= z
+        self.list = list_vaisseaux          ##### a continue
+        
+class Player():
+     def __init__(self,vessel_list: list , Arme_list: list , hits:int):
+     self.
