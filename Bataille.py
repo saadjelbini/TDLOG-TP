21e7d1de2c8fb3e353afb9f12be045219cfdc056
Jujgui <113675315+Jujgui@users.noreply.github.com>
2022-11-05 04:47:13 +0100

    Update Bataille.py

--- Bataille.py
+++ Bataille.py
@@ -1,6 +1,9 @@
-
 from Armes import*
 from vaisseaux import *
+
+import tkinter as tk
+from tkinter import ttk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 class espace_jeux():
     def __init__(self,x,y,z,list_vaisseaux):
